b0265c5549fe3f88115724452eaaf2a51aff5622
Favour Atsulighan <atsulighanfavour@gmail.com>
2026-07-15 23:41:04 +0100

    initilize commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
-import React from 'react'
+import React from "react";
 
-  
-
-export default function App() {
+export default function Gallery() {
   return (
-    
-
-     <img
-      src="https://react.dev/images/docs/scientists/MK3eW3Am.jpg"
-      alt="Katherine Johnson"
-    />
-
-  
-   
-  )
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      {/* <Profile />
+      <Profile /> */}
+    </section>
+  );
 }
-  ;
 
+function Profile() {
+  return (
+    // <PageLayout>
+    //   <NavigationHeader>
+    //     <SearchBar />
+    //     <Link to="/docs">Docs</Link>
+    //   </NavigationHeader>
+    //   <Sidebar />
+    //   <PageContent>
+    //     <TableOfContents />
+    //     <DocumentationText />
+    //   </PageContent>
+    // </PageLayout>
+
+    <div>
+      <img
+        src="https://react.dev/images/docs/scientists/MK3eW3As.jpg"
+        alt="Katherine Johnson"
+      />
+      <img
+        src="https://react.dev/images/docs/scientists/MK3eW3As.jpg"
+        alt="Katherine Johnson"
+      />
+      <img
+        src="https://react.dev/images/docs/scientists/MK3eW3As.jpg"
+        alt="Katherine Johnson"
+      />
+    </div>
+  );
+}
