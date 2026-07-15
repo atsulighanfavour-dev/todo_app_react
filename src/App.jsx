e7725483e84786b61a1ee82a7cedda045401e030
Favour Atsulighan <atsulighanfavour@gmail.com>
2026-07-15 23:25:48 +0100

    initilize commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+  
+
 export default function App() {
   return (
-    <div>
-      <div className="">Hello, World!</div>
-    </div>
+    
+
+     <img
+      src="https://react.dev/images/docs/scientists/MK3eW3Am.jpg"
+      alt="Katherine Johnson"
+    />
+
+  
+   
   )
 }
+  ;
+
